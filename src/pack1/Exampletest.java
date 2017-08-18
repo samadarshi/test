2b85e330adedc7641f91624843f1405c888b2c1b
samad <samad@DESKTOP-PRT0ACF>
2017-08-18 15:58:45 +0530

    Change-Id: I7f93091df909193368b94e251a5d839675ee6641

---
+++ src/pack1/Exampletest.java
@@ -0,0 +1,13 @@
+package pack1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Exampletest 
+{
+	@Test
+	public void test()
+	{
+		Reporter.log("this s used to test");
+	}
+}
